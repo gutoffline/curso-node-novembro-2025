e11e35ba2f0ea35543239a7263d541657352e546
Guto Xavier <gutoffline@gmail.com>
2025-11-11 19:48:01 -0300

    api atualizada

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("olá")
+const express = require("express")
+const app = express()
+app.get('/dc', function (req, res) {
+    const herois = ['Mulher Maravilha', 'Superman', 'Batman']
+    res.send(herois)
+})
+
+app.get('/marvel', function (req, res) {
+    const herois = ['Homem Aranha','Capitã Marvel', 'Capitão America']
+    res.send(herois)
+})
+
+app.listen(3000)
+
+
